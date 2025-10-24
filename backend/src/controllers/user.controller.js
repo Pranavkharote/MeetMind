71606180d5904ce95457d01da414f971e2a7f0d3
Pranav Satish Kharote <pranavkharote2005@gmail.com>
2025-10-24 15:20:38 +0530

    feat: implement user registration with validation and password hashing

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,36 @@
+import { User } from "../models/user.model";
+import httpStatus from "http-status"
+import bcrypt, {hashash} from "bcrypt"
+
 const register = async (req, res) => {
-    const {user}
+    try{
+        const {name, username, password} = req.body;
+        const existingUser = await User.find({username});
+        if(!name || !username || !password){
+            return res.status(httpStatus.BAD_REQUEST).json({
+          
+                message: "All fields are required"
+            })
+        }
+        if(existingUser){
+            return res.status(httpStatus.FOUND).json({
+            
+                message: "User already exist"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            name: name, 
+            username: username,
+            password: hashedPassword
+        })
+
+        await newUser.save();
+        res.status(httpStatus.CREATED).json({
+         
+            message:"User Created!"
+        })
+    }
+
 }
