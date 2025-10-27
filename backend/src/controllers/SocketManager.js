1c1441f83a62d1b31b5900106b4d4dc82faf7840
Pranav Satish Kharote <pranavkharote2005@gmail.com>
2025-10-27 11:32:10 +0530

    feat: enhance socket management with chat message handling and user disconnect notifications

--- backend/src/controllers/SocketManager.js
+++ backend/src/controllers/SocketManager.js
@@ -33,8 +33,54 @@ const connectToSocket = (server) => {
       io.to(toId).emit("signal", socket.id, message);
     });
 
-    socket.on("chat-message", (data, sender) => {});
-    socket.on("disconnect", () => {});
+    socket.on("chat-message", (data, sender) => {
+      const [matchingRoom, found] = Object.entries(connections).reduce(
+        ([room, isFound], [roomKey, roomValue]) => {
+          if (!isFound && roomValue.includes(socket.id)) {
+            return [roomKey, true];
+          }
+          return [room, isFound];
+        },
+        ["", false]
+      );
+      if (found === true) {
+        if (messages[matchingRoom] === undefined) {
+          messages[matchingRoom] = [];
+        }
+        messages[matchingRoom].push({
+          sender: sender,
+          data: data,
+          "socket-id-sender": socket.id,
+        });
+        console.log("message", key, ":", sender, data);
+
+        connections[matchingRoom].array.forEach((element) => {
+          io.to(element).emit("chat-message", data, sender, socket.id);
+        });
+      }
+    });
+    socket.on("disconnect", () => {
+      const diffTime = Math.abs(timeOnline[socket.id] - new Date());
+      var key;
+      for (const [k, v] of JSON.parse(
+        JSON.stringify(Object.entries(connections))
+      )) {
+        for (let a = 0; a < v.length; ++a) {
+          key = k;
+
+          for (let a = 0; a < connections[key].length; ++a) {
+            io.to(connections[key][a]).emit("user-left", socket.id);
+          }
+          var index = connections[key].indexOf(socket.id);
+
+          connections[key].splice(index, 1);
+
+          if (connections[key].length === 0) {
+            delete connections[key];
+          }
+        }
+      }
+    });
   });
 };
 
