b67c7a8f9d1b907afb47e1edfbfc582c4954ed50
Pranav Satish Kharote <pranavkharote2005@gmail.com>
2025-10-27 10:55:30 +0530

    feat: add user authentication with login and registration endpoints

--- backend/src/controllers/SocketManager.js
+++ backend/src/controllers/SocketManager.js
@@ -1,8 +1,41 @@
 import { Server } from "socket.io";
 
+let connections = {};
+let messages = {};
+let timeOnline = {};
+
 const connectToSocket = (server) => {
-    const io = new Server(server);
-    return io;
-}
+  const io = new Server(server);
+
+  io.on("connection", (socket) => {
+    socket.on("join-call", (path) => {
+      if (connections[path] === undefined) {
+        connections[path] = [];
+      }
+      connections[socket.id] = new Date();
+
+      for (let a = 0; a < connections[path].length; a++) {
+        io.to(connectToSocket[path][a]).emit("user-joined", socket.id);
+      }
+      if (messages[path] !== undefined) {
+        for (let a = 0; a < messages[path].length; ++a) {
+          io.to(socket.id).emit(
+            "chat-message",
+            messages[path][a]["data"],
+            messages[path][a]["sender"],
+            messages[path][a]["socket-id-sender"]
+          );
+        }
+      }
+    });
+
+    socket.on("signal", (toId, message) => {
+      io.to(toId).emit("signal", socket.id, message);
+    });
+
+    socket.on("chat-message", (data, sender) => {});
+    socket.on("disconnect", () => {});
+  });
+};
 
 export default connectToSocket;
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,36 +1,63 @@
-import { User } from "../models/user.model";
-import httpStatus from "http-status"
-import bcrypt, {hashash} from "bcrypt"
+import { User } from "../models/user.model.js";
+import httpStatus from "http-status";
+import bcrypt from "bcrypt";
+import crypto from "crypto"
+
+const login = async (req, res) => {
+    const {username, password} = req.body;
+    if(!username || !password){
+      return res.status(httpStatus.BAD_REQUEST).json({message: "All fields are required."})
+    }
+    try{
+      const user = await User.findOne({username});
+      if(!user){
+        return res.status(httpStatus.NOT_FOUND).json({message: "User not found"})
+      }
+      
+      if(bcrypt.compare(password, user.password)){
+        let token = crypto.randomBytes(20).toString("hex");
+
+        user.token = token;
+        await user.save();
+        return res.status(httpStatus.OK).json({token: token, message: "LoggedIn Successfully"})
+      }
+  }
+  catch(e){
+    return res.status(500).json({message: `something went wrong. ${e}`})
+  }
+}
 
 const register = async (req, res) => {
-    try{
-        const {name, username, password} = req.body;
-        const existingUser = await User.find({username});
-        if(!name || !username || !password){
-            return res.status(httpStatus.BAD_REQUEST).json({
-          
-                message: "All fields are required"
-            })
-        }
-        if(existingUser){
-            return res.status(httpStatus.FOUND).json({
-            
-                message: "User already exist"
-            })
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({
-            name: name, 
-            username: username,
-            password: hashedPassword
-        })
-
-        await newUser.save();
-        res.status(httpStatus.CREATED).json({
-  
-            message:"User Created!"
-        })
+    const { name, username, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (!name || !username || !password) {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        message: "All fields are required",
+      });
+    }
+    if (existingUser) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: "User already exist",
+      });
     }
 
-}
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name: name,
+      username: username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(httpStatus.CREATED).json({
+      message: "User Created Successfully!",
+    });
+  } catch (e) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: `Something went wrong ${e}`,
+    });
+  }
+};
+
+export {login, register}
